1239fc86f8473b5627bb6ab9b98f4739e92890dc
Dima Perepelica <dima.perepelica48@gmail.com>
2020-11-12 00:40:09 +0200

    releasev v1

---
+++ README.md
@@ -0,0 +1,11 @@
+# Avia-tickets-js
+An avia-tickets project for sale
+
+## Used API
+https://aviasales-api.herokuapp.com
+
+## Project setup
+```
+npm run dev
+```
+
---
+++ src/js/app.js
@@ -0,0 +1,41 @@
+import '../css/style.css';
+import './plugins';
+import ticketsUI from "./views/tickets";
+import locations from "./store/location";
+import formUI from "./views/form";
+import currencyUI from "./views/currency";
+
+document.addEventListener('DOMContentLoaded', () => {
+    initApp();
+    const form = formUI.form;
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        onFormSubmit();
+    });
+
+    async function initApp() {
+        await locations.init();
+        formUI.setAutocompleteData(locations.shortCitiesList);
+   }
+
+   async function onFormSubmit() {
+       const origin = locations.getCityCodeByKey(formUI.originValue);
+       const destination = locations.getCityCodeByKey(formUI.destinationValue);
+       const depart_date = formUI.departDateValue;
+       const return_date = formUI.returnDateValue;
+       const currency = currencyUI.currencyValue;
+
+       await locations.fetchTickets({
+            origin,
+            destination,
+            depart_date,
+            return_date,
+            currency,
+       });
+
+        ticketsUI.renderTickets(locations.lastSearch)
+    }
+
+});
+
